aa790c4a95d953908abb793152cf47ca43eea4bf
dhwani2 <68865611+dhwani2@users.noreply.github.com>
2020-08-04 21:49:06 -0400

    Add files via upload

---
+++ week1/day4q1.js
@@ -0,0 +1,10 @@
+/*
+Write a program that prints the first n elements of an array.
+*/
+
+flowers = ["daisy", "daffodil", "rose", "delilah"]
+
+console.log(flowers[0], flowers[1], flowers[2]);
+
+
+
